b5d3821cf373478d7df61d30f4a08babe251265d
Pimarla <pimarla.suw@student.mahidol.ac.th>
2026-04-05 18:56:39 +0700

    update

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ db.connect((err) => {
   }
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static('public'));
 app.use(router);
 
